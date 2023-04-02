bddc392b6bfd2cc70bc304815d0850263cbbc7c2
isyel <isykaal4saviour@gmail.com>
2023-04-02 15:35:24 +0100

    update firebase configs

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
 import firebase from "firebase";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-  projectId: "disneyplus-clone-a33d5",
-  storageBucket: "disneyplus-clone-a33d5.appspot.com",
-  messagingSenderId: "37918794208",
-  appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-  measurementId: "G-DRVLJKWRWG",
+  apiKey: "AIzaSyCu-CiLMws6j8lF5r9fY9p7ifZONbWvlF0",
+  authDomain: "disneyplus-clone-b6227.firebaseapp.com",
+  projectId: "disneyplus-clone-b6227",
+  storageBucket: "disneyplus-clone-b6227.appspot.com",
+  messagingSenderId: "72625548485",
+  appId: "1:72625548485:web:8f2ef1ee949708c387d4ac",
+  measurementId: "G-MZWGHY89BG",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const storage = firebase.storage();
 
-export { auth, provider, storage };
+export { auth, provider, storage, analytics };
 export default db;
